6217fd25dc951612dc05d12612377235994785f5
darshan magare <darshan.magare1234@gmail.com>
2021-10-16 10:39:57 +0530

    Selenium POM Framework Pushed

---
+++ SeleniumPOMFramework/src/main/java/utilities/ConstantValue.java
@@ -0,0 +1,9 @@
+package utilities;
+
+public class ConstantValue {
+	public static final String userName="mrdarshan1997@rediffmail.com";
+	public static final String password="Omsai@5115";
+	//public static final String userName="anshulc55";
+	//public static final String password="Test@1234";
+
+}
